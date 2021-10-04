6bd5c545c2cdb2eda3e57c19e4ef3449e3b39631
Keerbi Liu <kirby.liu12@gmail.com>
2021-10-04 14:52:29 +1100

    Created card columns on the right side, named elements for js.

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const inputEl = document.getElementById("cityInput");
+const searchEl = document.getElementById("citySearch");
+const cityEl = document.getElementById("city");
+const tempEl = document.getElementById("temperature");
+const windEl = document.getElementById("wind");
+const humidEl = document.getElementById("humidity");
+const uvEl = document.getElementById("uvIndex");
+
+
+
+const APIKey = "0a74b588522c230378c987063ead331b";
